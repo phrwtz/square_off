314975705c246243cebd26c7798f8b0421906f4b
Paul Horwitz <phorwitz@concord.org>
2019-11-10 21:44:43 -0500

    Added draw method to board

--- init.js
+++ init.js
@@ -1,9 +1,6 @@
 function Board(size, boxes, init, draw) {
     this.size = size;
     this.boxes = [];
-    this.draw = function () {
-        console.log("drawing board of size " + this.size);
-    }
     this.init = function () {
         for (let j = 0; j < size; j++) {
             for (let i = 0; i < size; i++) {
@@ -12,16 +9,32 @@ function Board(size, boxes, init, draw) {
                     thisBox.color = "white";
                     thisBox.rect.setAttribute("fill", "#FFFFFF");
                 } else {
-                thisBox.color = "wheat";
+                    thisBox.color = "wheat";
                     thisBox.rect.setAttribute("fill", "#F5DEB3");
                 }
                 this.boxes.push(thisBox);
             }
         }
+    };
+    this.draw = function () {
+        var container = document.getElementById("cont");
+        var outerRect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        outerRect.setAttribute("fill", "none");
+        outerRect.setAttribute("stroke", "black");
+        outerRect.setAttribute("stroke-width", "3");
+        outerRect.setAttribute("x", "40");
+        outerRect.setAttribute("y", "40");
+        outerRect.setAttribute("width", (this.size * 40).toString());
+        outerRect.setAttribute("height", (this.size * 40).toString());
+        container.appendChild(outerRect);
+        for (let i = 0; i < this.boxes.length; i++) {
+            container.appendChild(this.boxes[i].rect);
+        }
     }
 }
 
 var myBoard = new Board(12);
+myBoard.init();
 
 function Box(x, y, rect) {
     this.x = x;
@@ -30,4 +43,8 @@ function Box(x, y, rect) {
     this.rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
     this.rect.setAttribute("width", "40");
     this.rect.setAttribute("height", "40");
+    this.rect.setAttribute("x", ((x + 1) * 40).toString());
+    this.rect.setAttribute("y", ((y + 1) * 40).toString());
+    this.rect.setAttribute("stroke", "black");
+    this.rect.setAttribute("stroke-width", "1");
 }
