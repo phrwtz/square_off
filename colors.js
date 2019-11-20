21feb7cae366a6e14238a352dec708bc9bbecb17
Paul Horwitz <phorwitz@concord.org>
2019-11-19 20:35:54 -0500

    Added rgba color values

--- colors.js
+++ colors.js
@@ -1,6 +1,6 @@
 colorValue = {};
 colorValue["white"] = ["#FFFFFF", "#F5DEB3"];
-colorValue["red"] = ["#F06550","#FF0000"];
-colorValue["blue"] = ["#6495ED", "#0000FF"];
-colorValue["lightred"] = ["#FFC0CB", "#ff00ff"];
-colorValue["lightblue"] = ["ADD8E6", "#87CEFA"];
+colorValue["red"] = ["rgba(255,0,0,0.65)","rgba(255,0,0,1.0)"];
+colorValue["blue"] = ["rgba(0,0,255,0.65)", "rgba(0,0,255,1.0"];
+colorValue["lightred"] = ["rgba(255,192,203,0.65","rgba(255,192,203,1.0)"];
+colorValue["lightblue"] = ["rgba(135,206,250,0.65","rgba(135,206,250,1.0"];
