c58e6ed1acf4c9accf0def82ccc889b5b1d27727
Paul Horwitz <phorwitz@concord.org>
2019-11-20 17:43:14 -0500

    Added fill squares

--- colors.js
+++ colors.js
@@ -1,6 +1,6 @@
 colorValue = {};
 colorValue["white"] = ["#FFFFFF", "#F5DEB3"];
-colorValue["red"] = ["rgba(255,0,0,0.65)","rgba(255,0,0,1.0)"];
-colorValue["blue"] = ["rgba(0,0,255,0.65)", "rgba(0,0,255,1.0"];
-colorValue["lightred"] = ["rgba(255,192,203,0.65","rgba(255,192,203,1.0)"];
-colorValue["lightblue"] = ["rgba(135,206,250,0.65","rgba(135,206,250,1.0"];
+colorValue["red"] = ["rgba(255,0,0,0.7)","rgba(255,0,0,1.0)"];
+colorValue["blue"] = ["rgba(0,0,255,0.7)", "rgba(0,0,255,1.0"];
+colorValue["lightred"] = ["rgba(255,192,203,.7","rgba(255,192,203,1.0)"];
+colorValue["lightblue"] = ["rgba(135,206,250,0.7","rgba(135,206,250,1.0"];
