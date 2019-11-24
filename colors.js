8d79f5915a3685133d024a0de2ea3c137b95eefd
Paul Horwitz <phorwitz@concord.org>
2019-11-24 12:21:00 -0500

    fixed order of light red colors

--- colors.js
+++ colors.js
@@ -1,8 +1,8 @@
 colorValue = {};
 colorValue.white = ["#FFFFFF", "	#FFF8DC"];
-colorValue.red = ["#FF4070","#FF0000"];
+colorValue.red = ["#FA4070","#FF0000"];
 colorValue.blue = ["#3333ff", "#0010BB"];
-colorValue.lightred = ["#ff99cc","#ffb3d9"];
+colorValue.lightred = ["#ffb3d9","#ff99cc"];
 colorValue.lightblue = ["#B0E0E6", "#87CEEB"];
 
 function toggleCorners(corners) {
